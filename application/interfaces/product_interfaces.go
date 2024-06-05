7683ce910dc0d92ffc3202f9a0da5c37c8b63ee5
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-06-04 22:36:30 -0300

    feat: defines interfaces to ensure decoupling

---
+++ application/interfaces/product_interfaces.go
@@ -0,0 +1,31 @@
+package interfaces
+
+type ProductReader interface {
+	Get(id string) (ProductInterface, error)
+}
+
+type ProductWriter interface {
+	Save(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductPersistenceInterface interface {
+	ProductReader
+	ProductWriter
+}
+
+type ProductServiceInterface interface {
+	Get(id string) (ProductInterface, error)
+	Create(name string, price float64) (ProductInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
