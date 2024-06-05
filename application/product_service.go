acd82b3d3ba078e5e0be2937560b8b9f3dc865f5
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-06-04 23:34:46 -0300

    feat: ensure service implements interface

--- application/product_service.go
+++ application/product_service.go
@@ -2,9 +2,26 @@ package application
 
 import (
 	"github.com/DerivedPuma7/go-hexagonal/application/interfaces"
-	
 )
 
 type ProductService struct {
 	Persistence interfaces.ProductPersistenceInterface
 }
+
+var _ interfaces.ProductServiceInterface = (*ProductService)(nil)
+
+func (s *ProductService) Get(id string) (interfaces.ProductInterface, error) {
+
+}
+
+// func (s *ProductService) Create(name string, price float64) (interfaces.ProductInterface, error) {
+
+// }
+
+// func (s *ProductService) Enable(product interfaces.ProductInterface) (interfaces.ProductInterface, error) {
+
+// }
+
+// func (s *ProductService) Disable(product interfaces.ProductInterface) (interfaces.ProductInterface, error) {
+
+// }
