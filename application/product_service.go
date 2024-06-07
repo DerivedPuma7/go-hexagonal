cfde4ba06e6a161b33fd7dadde61724dfe05f3d3
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-06-06 21:19:38 -0300

    feat: concrete implementation of product service

--- application/product_service.go
+++ application/product_service.go
@@ -11,17 +11,52 @@ type ProductService struct {
 var _ interfaces.ProductServiceInterface = (*ProductService)(nil)
 
 func (s *ProductService) Get(id string) (interfaces.ProductInterface, error) {
-
+	product, err := s.Persistence.Get(id)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
 }
 
-// func (s *ProductService) Create(name string, price float64) (interfaces.ProductInterface, error) {
+func (s *ProductService) Create(name string, price float64) (interfaces.ProductInterface, error) {
+	product := NewProduct()
+	product.Name = name
+	product.Price = price
 
-// }
+	_, err := product.IsValid()
+	if err != nil {
+		return &Product{}, err
+	}
 
-// func (s *ProductService) Enable(product interfaces.ProductInterface) (interfaces.ProductInterface, error) {
+	result, err := s.Persistence.Save(product)
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
+}
 
-// }
+func (s *ProductService) Enable(product interfaces.ProductInterface) (interfaces.ProductInterface, error) {
+	err := product.Enable()
+	if err != nil {
+		return &Product{}, err
+	}
+	
+	result, err := s.Persistence.Save(product);
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
+}
 
-// func (s *ProductService) Disable(product interfaces.ProductInterface) (interfaces.ProductInterface, error) {
-
-// }
+func (s *ProductService) Disable(product interfaces.ProductInterface) (interfaces.ProductInterface, error) {
+	err := product.Disable()
+	if err != nil {
+		return &Product{}, err
+	}
+	
+	result, err := s.Persistence.Save(product);
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
+}
