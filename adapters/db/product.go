582388feb018399152763643097ab90fbcd0c260
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-06-12 20:37:34 -0300

    feat: implementing adapter to ProductPersistenceInterface

---
+++ adapters/db/product.go
@@ -0,0 +1,79 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/DerivedPuma7/go-hexagonal/application"
+	"github.com/DerivedPuma7/go-hexagonal/application/interfaces"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db:db}
+}
+
+var _ interfaces.ProductPersistenceInterface = (*ProductDb)(nil)
+
+func (p *ProductDb) Get(id string) (interfaces.ProductInterface, error) {
+	var product application.Product
+	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (p *ProductDb) Save(product interfaces.ProductInterface) (interfaces.ProductInterface, error) {
+	var rows int
+	p.db.QueryRow("select id from products where id=?", product.GetID()).Scan(&rows)
+	if rows == 0 {
+		_, err := p.create(product)
+		if err != nil {
+			return nil, err
+		}
+		return product, nil
+	}
+	_, err := p.update(product)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *ProductDb) create(product interfaces.ProductInterface) (interfaces.ProductInterface, error) {
+	stmt, err := p.db.Prepare(`insert into products(id, name, price, status) values(?, ?, ?, ?)`)
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmt.Exec(
+		product.GetID(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *ProductDb) update(product interfaces.ProductInterface) (interfaces.ProductInterface, error) {
+	_, err := p.db.Exec(`update products set name=?, price=?, status=? where id=?`, product.GetName(), product.GetPrice(), product.GetStatus(), product.GetID())
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
