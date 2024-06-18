6ce71a6e1ccce77ecc99667456b0e4ce0a821a25
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-06-17 21:35:35 -0300

    feat: implement http server and route handler

---
+++ adapters/web/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"github.com/DerivedPuma7/go-hexagonal/adapters/web/handler"
+	"github.com/DerivedPuma7/go-hexagonal/application/interfaces"
+
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+type Webserver struct {
+	Service interfaces.ProductServiceInterface
+}
+
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Serve() {
+	r := mux.NewRouter() // routes -> similar to express
+	n := negroni.New( // middlewares
+		negroni.NewLogger(),
+	)
+
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr: ":8080",
+		Handler: http.DefaultServeMux,
+		ErrorLog: log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
